e9989eee83597cbb570375a22a8ffc299129d2b2
Eric Chahin <erc73@cornell.edu>
2014-11-13 00:55:55 -0500

    selection methods

--- evolve_music.py
+++ evolve_music.py
@@ -11,6 +11,7 @@ from logger import Logger
 import read_music
 from IPython.core.debugger import Tracer
 import AudioBite
+import copy
 
 #----- GLOBALS ----------
 
@@ -85,21 +86,41 @@ def main():
 
 # Hill climber
 def mutation(sound):
+  ''' Takes a random note and changes it. Doesn't hurt (mutate) the parent. '''
+  sound = copy.deepcopy(sound)
   # take a random note and change it
   random_i = random.randint(0,len(sound.note_list)-1)
-  # pick random note
+  # TODO change this later... assumes that 
+  notes_to_pick_from = ['C5','Db5','D5','Eb5','E5','F5','Gb5','G5','Ab5','A5','Bb5','B5']
+  random_note = random.choice(notes_to_pick_from)
+  random.shuffle(sound.note_list[i])
 
-  # Maybe change its window?
-  pass
+  if len(sound.note_list[i]) > 0 and random.randint(0,1): # either delete or not.
+    sound.note_list[i].pop
+  elif random.randint(0,1): # either add a note or not
+    sound.note_list[i].append(random_note)
+  return sound
 
-
-# 
 def crossover(dad,mom):
   '''
   Takes two songs and performs a 2 slice crossover to return two songs.
+  Does not harm mom or dad. :)
   '''
   # Take two random slices and then swap
-  pass
+  son      = copy.deepcopy(dad)
+  daughter = copy.deepcopy(mom)
+  assert len(son.note_list) == len(daughter.note_list)
+  random_i = random.randint(0,len(son.note_list))
+  random_j = random.randint(0,len(son.note_list))
+  if random_i > random_j:
+    tmp = random i
+    random_i = random_j
+    random_j = tmp
+  son_middle = son.note_list[random_i:random_j]
+  daughter_middle = daughter.note_list[random_i:random_j]
+  son.note_list = son.note_list[0:random_i] + daughter_middle + son.note_list[random_j:]
+  daughter.note_list = daughter.note_list[0:random_i] + son_middle + daughter.note_list[random_j:]
+  return (son,daughter)
 
 # ----- SELECTION METHODS ----------------
 
@@ -138,6 +159,20 @@ class EvolvedSound(object):
   def save_sound(self,filename):
     halfbeat = float(self.period)/NUMBER_OF_HALFBEATS_PER_PERIOD
     read_music.save_representation_as_song(self,SAVE_DIR_FOR_SOUNDS,filename,halfbeat)
+
+# ------ NOTE METHODS ------------
+
+def moveNote(note_str,n):
+  ''' Moves note n halfsteps up or down from current spot note_str'''
+  scale = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
+  octave = int(note_str[-1])
+  note = note_str[:2] if len(note_str) == 3 else note_str[:1]
+  i = scale.index(note)
+  new_note = scale[(i + n) % len(scale)]
+  new_octave = octave + (i + n) / len(scale)
+  return str(new_note) + str(new_octave)
+
+  
 
 if __name__ == '__main__':
   main()
